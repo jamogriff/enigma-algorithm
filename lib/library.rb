095d630aee260925e27b08822922763c382fca46
Jamison Griffith <jamison.griffith@gmail.com>
2021-04-22 12:45:42 -0600

    Add character set and test file

--- lib/library.rb
+++ lib/library.rb
@@ -1,5 +1,9 @@
 class Library
 
+  def self.character_set
+    ("a".."z").to_a << " "
+  end
+  
   def self.code_book
     { A: 1, B: 2,
       C: 3, D: 4 }
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -2,6 +2,9 @@ require './lib/library'
 
 RSpec.describe Library do
 
+  it 'contains character set' do
+    expect(Library.character_set.length).to eq 27
+  end
   it 'houses reference to underlying shift algorithm' do
     expect(Library.code_book[:B]).to eq 2
     expect(Library.code_book[:D]).to eq 4
