44bea2179555729f1b1c191936b0750bdf976128
Jamison Griffith <jamison.griffith@gmail.com>
2021-04-22 15:47:20 -0600

    Add create_date_code method with verified function

--- lib/offset_generator.rb
+++ lib/offset_generator.rb
@@ -1,40 +1,38 @@
 class OffsetGenerator
-  attr_reader :date
+  attr_reader :date_code
 
   def initialize(date)
-    @date = date.to_s
+    @date_code = create_date_code(date)
   end
 
-
-
-  def parse_input_date
-    corrected_digits = date[2..-1]
-    ordered_array = corrected_digits.split('-').reverse
-    ordered_array.join.to_i
+  def create_date_code(date)
+    squared = parse_input_date(date)**2
+    trimmed_chars = squared.to_s[-4..-1]
+    trimmed_chars.split(//).map do |char|
+      char.to_i
+    end
   end
-
-  def date_code
-    square = parse_input_date**2
-    trimmed_chars = square.to_s[-4..-1]
-    trimmed_chars.to_i
-  end
-
-
 
   def generate(shift_type)
     case shift_type
     when :A
-
+      date_code[0]
     when :B
-
+      date_code[1]
     when :C
-
+      date_code[2]
     when :D
-
+      date_code[3]
     else
-      "Lol build l8r"
+      0
     end
   end
 
+  def parse_input_date(date)
+    date_string = date.to_s
+    corrected_digits = date_string[2..-1]
+    ordered_array = corrected_digits.split('-').reverse
+    ordered_array.join.to_i
+  end
 
 end
--- spec/offset_generator_spec.rb
+++ spec/offset_generator_spec.rb
@@ -3,23 +3,27 @@ require './lib/offset_generator'
 RSpec.describe OffsetGenerator do
   date = Date.new(1995, 8, 4)
   offset_gen = OffsetGenerator.new(date)
+  expected_code = [1, 0, 2, 5]
 
   it 'returns a date' do
-    expect(offset_gen.date).to be_instance_of String
+    expect(offset_gen.date_code).to be_instance_of Array
   end
 
   it 'parses a date into an integer code' do
-    expect(offset_gen.parse_input_date).to eq 40895
+    expect(offset_gen.parse_input_date(date)).to eq 40895
   end
 
   it 'returns four digit code number' do
     allow(offset_gen).to receive(:parse_input_date) do
       40895
     end
-    expect(offset_gen.date_code).to eq 1025
+    expect(offset_gen.create_date_code(date)).to eq expected_code
   end
 
-  xit 'returns valid offsets' do
+  it 'returns valid offsets' do
+    allow(offset_gen).to receive(:date_code) do
+      expected_code
+    end
     expect(offset_gen.generate(:A)).to eq 1
     expect(offset_gen.generate(:B)).to eq 0
     expect(offset_gen.generate(:C)).to eq 2
